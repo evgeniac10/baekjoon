6430ce985832c09e6aeeb89ec2d2848ae3c65c9e
evgeniac10 <rlagustjd2684@gmail.com>
2023-01-28 17:17:00 +0900

    백준 3단계 합
    백준 1문제 풀이

--- 백준_3단계/백준_3단계_합.java
+++ 백준_3단계/백준_3단계_합.java
@@ -7,14 +7,16 @@ public class 백준_3단계_합 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
-		int x = sc.nextInt();
+		int plus = sc.nextInt();
 		int sum = 0 ;
-		for(int i = 0; i< x; i++) {
-			 sum = sum + x ;
-			System.out.print(sum);ㅁㄴ
+		int save  =0 ;
+		
+		for(int i = 0; i< plus; i++) {
+			save = i + 1;
+			sum = save+sum ;
+		
+			
 		}
-
+		 System.out.println(sum);
 	}
-
 }
