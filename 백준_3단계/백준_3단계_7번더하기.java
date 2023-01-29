5b708f34d6315b8e140fd31b034e473c1f502eeb
evgeniac10 <rlagustjd2684@gmail.com>
2023-01-29 19:13:03 +0900

    백준 3단계 2문제
    백준 3단계

--- 백준_3단계/백준_3단계_7번더하기.java
+++ 백준_3단계/백준_3단계_7번더하기.java
@@ -1,10 +1,22 @@
 package 백준_3단계_;
 
+import java.util.Scanner;
+
 public class 백준_3단계_7번더하기 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int T = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=T; i++) {
+			int num_i = sc.nextInt();
+			int num2_i = sc.nextInt();
+			sum = num_i + num2_i;
+			System.out.println("Case #" + i +": "+ sum);
+		}
 	}
 
 }
--- 백준_3단계/백준_3단계_8번더하기.java
+++ 백준_3단계/백준_3단계_8번더하기.java
@@ -1,10 +1,24 @@
 package 백준_3단계_;
 
+import java.util.Scanner;
+
 public class 백준_3단계_8번더하기 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int num_i ;
+		int num2_i;
+		int sum ;
+		int i ;
+		for(i=1; i<=T; i++) {
+			 num_i = sc.nextInt();
+			 num2_i = sc.nextInt();
+			 sum = num_i + num2_i;
+				System.out.println("Case #" + i + ": " + num_i + " + " + num2_i + " = "+ sum);
 
+		}
+		
 	}
 
 }
