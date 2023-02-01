1fdf337889b35b712c54d852cd3bffb659583d47
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-02 01:10:27 +0900

    set을 이용해서 풀기

--- 백준_4단계/백준_4단계_나머지.java
+++ 백준_4단계/백준_4단계_나머지.java
@@ -1,10 +1,21 @@
 package 백준_4단계_;
 
+import java.util.*;
+
 public class 백준_4단계_나머지 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		for(int i=0; i<10; i++)
+		{
+			int num =sc.nextInt();
+			set.add(num % 42);
+		}
+		System.out.print(set.size());
 	}
-
 }
