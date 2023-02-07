551a7f2c0408c521cec8539a499862d96f23903f
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-07 22:10:24 +0900

    백준 7단계 풀이

--- 백준_7단계_/백준_7단계_벌집.java
+++ 백준_7단계_/백준_7단계_벌집.java
@@ -1,9 +1,36 @@
 package 백준_7단계_;
 
+import java.util.Scanner;
+
 public class 백준_7단계_벌집 {
 
 	public static void main(String[] args) {
-	
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int room = 1;
+		int save = 2;
+		
+		if(N == 1) 
+		{
+			System.out.println(1);
+		}
+		else 
+		{
+			while(true) 
+			{
+				if(N >= save) 
+				{
+				save = save + (room*6) ;
+				room++;
+				}
+				else 
+				{
+				System.out.println(room);
+				break;
+				}
+			}
+		}
+		sc.close();
 	}
-
 }
