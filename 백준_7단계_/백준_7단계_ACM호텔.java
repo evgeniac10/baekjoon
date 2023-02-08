d4e3f1a315e676df41019c0c25bd101569bdf5c3
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-08 20:39:49 +0900

    백준_7단계_acm호텔 풀이

--- 백준_7단계_/백준_7단계_ACM호텔.java
+++ 백준_7단계_/백준_7단계_ACM호텔.java
@@ -1,33 +1,24 @@
 package 백준_7단계_;
 import java.util.*;
-//unit 호수 , floor 층
 public class 백준_7단계_ACM호텔 {
-
-	static int floor(int first , int second , int third) 
-	{
-		int unit = (third / first) + 1;
-		int floor =(third % first) ;
-		
-		return unit + floor* 100;
-	}
-
-	
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
 		int test = sc.nextInt();
-		int H = sc.nextInt();
-		int W = sc.nextInt();
-		int N = sc.nextInt();
-		int H_2 = sc.nextInt();
-		int W_2 = sc.nextInt();
-		int N_2 = sc.nextInt();
 		
-		System.out.println(floor(H,W,N));
-		System.out.println(floor(H_2,W_2,N_2));
+		for(int i =0; i<test; i++) {
+			int floor = sc.nextInt();
+			int unit = sc.nextInt();
+			int N = sc.nextInt();
+			if((N%floor)==0) {
+				System.out.println(floor*100 + (N/floor) );
 
-	}
+			}
+			else {
+				System.out.println((N%floor)*100 + (N/floor+1) );
+
+			}
+		}
+	sc.close();}
 
 }
