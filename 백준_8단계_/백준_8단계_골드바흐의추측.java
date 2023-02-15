6e06c1f2b14bb218d3b4835ad434b53faa06f6df
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-15 23:52:13 +0900

    백준 8단계 마무리

--- 백준_8단계_/백준_8단계_골드바흐의추측.java
+++ 백준_8단계_/백준_8단계_골드바흐의추측.java
@@ -3,23 +3,45 @@ package 백준_8단계_;
 import java.util.*;
 
 public class 백준_8단계_골드바흐의추측 {
+	static Boolean Prime(int num) {// 소수인지 소수가 아닌지 판별하는 메소드
+		if (num < 2) {
+			return false;
+		} else {
+			for (int i = 2; i * i <= num; i++) {
+				if (num % i == 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
-		for(int i=0; i<T; i++) {
-			int N = sc.nextInt();
-			int num = N/2 ;
-			if(N==4) {
-				System.out.println(2+" "+2);
-			}
-			else if(num%2 ==0){
-				System.out.println((N/2)-1 + " " + ((N/2)+1));
-			}
-			else {	
-				System.out.println((N/2) + " " + (N/2));
+
+		for (int k = 0; k < T; k++) {
+			int even = sc.nextInt() / 2;
+			int front = even;
+			int back = even;
+
+			if (Prime(even) == true) {
+				System.out.println(even + " " + even);
+			} else {
+				front--;
+				back++;
+				while (true) {
+					if (Prime(front) && Prime(back)) {
+						System.out.println(front + " " + back);
+						break;
+					}
+					else {
+						front--;
+						back++;
+					}
+				}
 			}
 		}
+		sc.close();
 	}
 }
