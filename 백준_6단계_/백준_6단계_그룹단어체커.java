ac7b0d86c6b8e85bbd5c63bd7e735d700b70bf63
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-17 20:07:02 +0900

    백준 6단계 그룹단어체커 알고리즘

--- 백준_6단계_/백준_6단계_그룹단어체커.java
+++ 백준_6단계_/백준_6단계_그룹단어체커.java
@@ -6,8 +6,23 @@ public class 백준_6단계_그룹단어체커 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+		int num = sc.nextInt();
+		int count = num;
+		for (int i = 0; i < num; i++) {
+			String str = sc.next(); // aabac
+			boolean arr[] = new boolean[26];
+
+			for (int j = 0; j < str.length() - 1; j++) {
+				if (j < str.length() - 1 && str.charAt(j) != str.charAt(j + 1)) {
+					if (arr[str.charAt(j + 1) - 97] == true) {
+						count--;
+						break;
+					}
+				}
+				arr[str.charAt(j) - 97] = true;
+			}
+		}
+		System.out.print(count);
 	}
 
 }
