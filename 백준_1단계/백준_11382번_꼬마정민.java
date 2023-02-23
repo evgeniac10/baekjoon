4bbac9b4f9b67bb31b8d72f6de0444659461a560
evgeniac10 <rlagustjd2684@gmail.com>
2023-02-23 23:16:14 +0900

    백준문제풀이
    꼬마정민 - 처음에 int타입으로 입력을 받으니 인풋미스매치라는 오류가 발생하여 타입을 Long타입으로 바꿔주니 해결이 되었다.

---
+++ 백준_1단계/백준_11382번_꼬마정민.java
@@ -0,0 +1,18 @@
+package 백준_1단계;
+
+import java.util.Scanner;
+
+public class 백준_11382번_꼬마정민 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+		
+		long sum = A + B + C;
+		System.out.print(sum);
+	
+	}
+
+}
