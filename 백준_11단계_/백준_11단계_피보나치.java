fdc6b4087eeff630bd26477a48004082e5b367c5
evgeniac10 <rlagustjd2684@gmail.com>
2023-03-06 23:08:03 +0900

    피보나치
    재귀 메소드를 이용하여 풀이가 가능하다.

--- 백준_11단계_/백준_11단계_피보나치.java
+++ 백준_11단계_/백준_11단계_피보나치.java
@@ -1,10 +1,15 @@
 package 백준_11단계_;
 
+import java.util.Scanner;
 public class 백준_11단계_피보나치 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int fibonacci(int num) {
+		if(num == 0 ) { return 0 ;}
+		if(num == 1 ) { return 1 ;}
+		return fibonacci(num - 2) + fibonacci(num - 1);
 	}
-
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println(fibonacci(sc.nextInt()));
+	}
 }
