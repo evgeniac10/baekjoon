5a137d97ccfeb207f3cb7d95fbe6757c553ce38c
evgeniac10 <rlagustjd2684@gmail.com>
2023-03-11 15:22:26 +0900

    Update 백준_11단계_하노이탑.java
    재귀를 이용하여 자리를 바꿔준다

--- 백준_11단계_/백준_11단계_하노이탑.java
+++ 백준_11단계_/백준_11단계_하노이탑.java
@@ -1,10 +1,33 @@
 package 백준_11단계_;
 
+import java.util.Scanner;
+
 public class 백준_11단계_하노이탑 {
 
+	public static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+
+		sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+
+		Hanoi(N, 1, 2, 3);
+		System.out.println(sb);
 
 	}
 
+	public static void Hanoi(int N, int start, int mid, int to) {
+		if (N == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
+		Hanoi(N - 1, start, to, mid);
+
+		sb.append(start + " " + to + "\n");
+
+		Hanoi(N - 1, mid, start, to);
+
+	}
 }
