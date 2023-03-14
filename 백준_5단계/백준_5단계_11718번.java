2fdf44d3c78f8fa2730974196b94b115e588123d
evgeniac10 <rlagustjd2684@gmail.com>
2023-03-14 18:56:25 +0900

    백준5단계_그대로출력
    무한 반복문에서 스캐너클래스에 있는 메소드를 이용하여 입력 받는 값이 없다면 더이상 반복문에서 나오게 하는 코드

---
+++ 백준_5단계/백준_5단계_11718번.java
@@ -0,0 +1,15 @@
+package 백준_5단계_;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 백준_5단계_11718번 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	sc.close();
+	}
+}
