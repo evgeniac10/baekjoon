c1c84a47b7f1a92c98c7e70bde82bf3c427620e0
evgeniac10 <rlagustjd2684@gmail.com>
2023-04-08 00:53:17 +0900

    후위표기식2 스택을 이용
    수정본

--- 백준/src/백준_후위표기식2_1935번.java
+++ 백준/src/백준_후위표기식2_1935번.java
@@ -8,22 +8,22 @@ public class 백준_후위표기식2_1935번 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt();
+//		int N = sc.nextInt(); //넥스트인트를 하면 숫자는 n으로 들어가지만 다음 문자열을 입력하기전에 개행을 하는데 개행도 입력에 포함되기에 다음 str에는 개행인 빈칸이 들어가게 된다.
+		int N = Integer.parseInt(sc.nextLine()); //넥스트 라인을 해야 입력 받은 숫자가 들어가고 개행을 무시할수 있다.
 		String str = sc.nextLine();
 
 		Stack<Double> st = new Stack<>();
-
+		double result = 0;
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
 
 			if (Character.isLetter(ch)) {
-				st.add((double) (str.charAt(i) - 'A'));
-				System.out.println(st.pop());
+				st.add((double) (str.charAt(i) - 'A'+1));
 			} else {
-				double number1 = st.pop();
 				double number2 = st.pop();
+				double number1 = st.pop();
 
-				double result = 0;
+				
 
 				switch (ch) {
 				case '+':
