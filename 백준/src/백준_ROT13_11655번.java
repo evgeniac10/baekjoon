7178efa7f7684c43ee480515eaa93560823e5b59
evgeniac10 <rlagustjd2684@gmail.com>
2023-04-19 23:50:35 +0900

    미완성
    띄어쓰기 와  숫자

---
+++ 백준/src/백준_ROT13_11655번.java
@@ -0,0 +1,36 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_ROT13_11655번 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String str = br.readLine();
+		
+		for(int i=0; i<str.length(); i++) {
+			 if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+	                if (str.charAt(i) >= 'N') {
+	                    sb.append((char)(str.charAt(i) - 13));
+	                } else {
+	                	sb.append((char)(str.charAt(i) + 13));
+	                }
+	            } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+	                if (str.charAt(i) >= 'n') {
+	                	sb.append((char)(str.charAt(i) - 13));
+	                } else {
+	                	sb.append((char)(str.charAt(i) + 13));
+	                }
+	            }
+	            else {
+	            	sb.append(" ");
+	            }
+		}
+		System.out.println(sb);
+	}
+
+}
