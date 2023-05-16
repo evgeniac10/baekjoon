74af6ae90fb621d7bed339d936a078dd1322a923
evgeniac10 <rlagustjd2684@gmail.com>
2023-05-17 00:31:46 +0900

    2차원 배열 완성본
    행과 열 헷갈리지 않기 !

--- 백준/src/백준_세로읽기_10789번.java
+++ 백준/src/백준_세로읽기_10789번.java
@@ -8,15 +8,26 @@ public class 백준_세로읽기_10789번 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		char arr[][] = new char[5][15];
+		int max = 0;
 		
-		char arr[][] = null;
 		for (int i = 0; i < 5; i++) {
 			String str = br.readLine();
-			for(int j=0; j<str.length(); j++) {
-				arr[i][j]= str.charAt(j);
+			
+			if (str.length() > max)
+				max = str.length();
+			for (int j = 0; j < str.length(); j++) {
+				arr[i][j] = str.charAt(j);
 			}
 		}
-
+		for(int k=0;k<max; k++) {
+			for(int l=0; l<5; l++ ) {
+				if(arr[l][k] =='\0')
+					continue;
+				sb.append(arr[l][k]);
+			}
+		}
+		System.out.println(sb);
 	}
-
 }
